e2c0786e84d34e3bf738d2c86b60605bdc40d536
MINI <MINI@DESKTOP-D306288>
2020-05-02 03:49:48 +0700

    Merge origin/master
    Conflicts:
    	src/GameLibrary.java
    	src/Test.java

--- Final62Project(new)/src/dataaccess/DBconnection.java
+++ Final62Project(new)/src/dataaccess/DBconnection.java
@@ -2,9 +2,8 @@ package dataaccess;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
 
 public class DBconnection {
 
--- Final62Project(new)/src/dataaccess/DBmanager.java
+++ Final62Project(new)/src/dataaccess/DBmanager.java
@@ -6,12 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import service.CustomerAccount;
 import service.TopupStatus;
-
 
 public class DBmanager {
 
@@ -96,23 +92,23 @@ public class DBmanager {
                     PreparedStatement pstm = con.prepareStatement(sql1);) {
                 for (int i = 0; i < ac.getMyCart().getItemInCart().size(); i++) {
                     double myEachMoney = dataaccess.DBmanager.SelectLastMoney(ac) - ac.getMyCart().getEachGamePrice(i);
-                     String sql3 = "UPDATE CUSTOMERACCOUNT set MYMONEY=" + myEachMoney + " WHERE id =" + ac.getUniqueId();
-            try (Statement stm = con.createStatement();) {
-                
-                stm.executeUpdate(sql3);}catch(SQLException ex){
-                
-                }
-                    pstm.setString(1, new TimeStamp().toString());
-                pstm.setDouble(2, ac.getUniqueId());
-                pstm.setString(3, ac.getUsername());
-                pstm.setString(4, ac.getMyCart().getItemInCart().get(i).getTitle());
-                pstm.setDouble(5, ac.getMyCart().getEachGamePrice(i));
-                pstm.setDouble(6, myEachMoney);
+                    String sql3 = "UPDATE CUSTOMERACCOUNT set MYMONEY=" + myEachMoney + " WHERE id =" + ac.getUniqueId();
+                    try (Statement stm = con.createStatement();) {
 
-                pstm.executeUpdate();
-                    
+                        stm.executeUpdate(sql3);
+                    } catch (SQLException ex) {
+
+                    }
+                    pstm.setString(1, new TimeStamp().toString());
+                    pstm.setDouble(2, ac.getUniqueId());
+                    pstm.setString(3, ac.getUsername());
+                    pstm.setString(4, ac.getMyCart().getItemInCart().get(i).getTitle());
+                    pstm.setDouble(5, ac.getMyCart().getEachGamePrice(i));
+                    pstm.setDouble(6, myEachMoney);
+
+                    pstm.executeUpdate();
+
                 }
-                
 
             } catch (SQLException ex) {
                 ex.getMessage();
@@ -234,7 +230,7 @@ public class DBmanager {
                 myMoney = rs.getDouble("MYMONEY");
             }
             System.out.println("========================================================================");
-            System.out.println(String.format("%10s %s %20s %s %20s %s %10s " , "ID", "|", "USERNAME", "|", "PASSWORD", "|", "MYMONEY"));
+            System.out.println(String.format("%10s %s %20s %s %20s %s %10s ", "ID", "|", "USERNAME", "|", "PASSWORD", "|", "MYMONEY"));
             System.out.println("========================================================================");
             System.out.println(String.format("%10s %s %20s %s %20s %s %10s ", id, "|", username, "|", password, "|", myMoney));
             System.out.println("------------------------------------------------------------------------");
@@ -243,7 +239,8 @@ public class DBmanager {
             System.out.println(ex.getMessage());
         }
     }
-        public static long selectLastCustomerID() {
+
+    public static long selectLastCustomerID() {
         long id = 0;
         try (Connection con = DBconnection.getConnecting();
                 Statement stm = con.createStatement();) {
@@ -264,5 +261,4 @@ public class DBmanager {
         return id;
     }
 
-    
 }
