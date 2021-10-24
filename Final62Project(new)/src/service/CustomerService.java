b4350b91fbb977d0819128b43b47de82c18d36bc
Kampol-Nui <kampol.43@mail.kmutt.ac.th>
2021-10-24 16:54:31 +0700

    refactor folder

--- Final62Project(new)/src/service/CustomerService.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-
-public interface CustomerService {
-    public abstract boolean addGameToCart(GameStore gameStore, String title);
-    public abstract boolean removeGameFromCart(String title);
-
-}
