8417a098c3049e83279308296062cf59aae662f1
Kampol-Nui <60166915+Kampol-Nui@users.noreply.github.com>
2021-10-24 16:36:51 +0700

    Delete Final62Project(newมากๆ) directory

--- Final62Project(newมากๆ)/src/service/CustomerService.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-
-public interface CustomerService {
-    public abstract boolean addGameToCart(GameStore gameStore, String title);
-    public abstract boolean removeGameFromCart(CustomerAccount ca, String title);
-
-}
